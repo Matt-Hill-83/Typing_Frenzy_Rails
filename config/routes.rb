4526ab11e340811a6e0d4379202cb99f24cba14d
KletusVandamme <matt.hill.email@gmail.com>
2015-03-08 00:41:26 -0800

    added static_pages controller and modified routes to allow slash to redirect to my static root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 NinetyNineCats::Application.routes.draw do
+
+  get 'static_pages/root'
+  #
+  # root :to => redirect('/test.html.erb')
+  root to: 'static_pages#root'
+
+  # get '/', :to => redirect('/views/root.html.erb')
+
   resources :cats, except: :destroy
   resources :cat_rental_requests, only: [:create, :new] do
     post "approve", on: :member
@@ -7,6 +15,7 @@ NinetyNineCats::Application.routes.draw do
   resource :session, only: [:create, :destroy, :new]
   resources :users, only: [:create, :new]
 
-  root to: redirect("/cats")
+  # root to: redirect("/cats")
+  # get ':action' => 'static#:action'
   # root to: redirect("/static_pages")
 end
